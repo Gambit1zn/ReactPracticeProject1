293f57f455ff19fdcb497eb9820941605f73545f
Ethan Ankadu <ethan.ankadu@gamesglobal.com>
2022-12-11 16:15:04 +0200

    added portals feature in react

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,12 +1,16 @@
 import Card from "./Card";
 import Button from "./Button";
 import classes from './ErrorModal.module.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const ErrorModal = props => {
+const Backdrop = props => {
+  return <div className={classes.backdrop} onClick={props.onConfirm}></div>
+}
+
+const ModalOverlay = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm}></div>
-      <Card className={classes.modal}>
+    <Card className={classes.modal}>
       <header className={classes.header}>
         <h2>{props.title}</h2>
       </header>
@@ -17,7 +21,15 @@ const ErrorModal = props => {
         <Button onClick={props.onConfirm}>Okay</Button>
       </footer>
     </Card>
-    </div>
+  );
+}
+
+const ErrorModal = props => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/>, document.getElementById('backdrop-root'))}
+      {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>, document.getElementById('modal-root'))}
+    </React.Fragment>
   );
 };
 
